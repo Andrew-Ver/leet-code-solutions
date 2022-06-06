49904b3e6e3ebd3e40797faeaa8a711ef4aed627
Andrew-Ver <41878065+Andrew-Ver@users.noreply.github.com>
2022-06-05 21:50:57 -0400

    Add solutions

--- 1461-string-contains-all-binary-codes-of-k.py
+++
@@ -1,6 +0,0 @@
-'''
-    https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
-'''
-
-def hasAllCodes(self, s: str, k: int) -> bool:
-    
--- 1971-find-if-path-exists-in-graph.js
+++ 1971-find-if-path-exists-in-graph.js
@@ -2,10 +2,15 @@
  * https://leetcode.com/problems/find-if-path-exists-in-graph/
  */
 
- var validPath = function(n, edges, source, destination) {
+
+var validPath = function(n, edges, source, destination) {
+    if (source == destination) {
+        return true;
+    }
+
     const adj_list = {};
 
-    for (let i=0; i<n; i++){
+    for (let i=0; i<edges.length; i++){
         let [A, B] = edges[i];
         
         if (A in adj_list){
@@ -21,14 +26,28 @@
             adj_list[B] = [A];
         }
     }
-    if (!(source in adj_list) || !(destination in adj_list)){
+    if (!(source in adj_list) || (!destination in adj_list)){
         return false;
     }
     
+    var explore = [source];
+    var seen = new Set();
 
+    while (explore.length) {
+        let node = explore.pop();
+        seen.add(node);
 
-
-    return adj_list;
+        for (let n of adj_list[node]) {
+            if (!seen.has(n)) {
+                explore.push(n);
+            }
+        }
+        
+        if (node == destination) {
+            return true;
+        }
+    }
+    return false;
 };
 
-console.log(validPath(3, [[3, 1], [1, 2], [2, 0]], 0, 2));
+console.log(validPath(10, [[2,9],[7,8],[5,9],[7,2],[3,8],[2,8],[1,6],[3,0],[7,0],[8,5]], 1, 0));
